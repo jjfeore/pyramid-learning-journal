e3c2e15ca1c897c4a41ed59158b11b3fb36dfe27
Morgan Nomura <morganelle@gmail.com>
2017-05-31 15:54:26 -0700

    debug templates

--- learning_journal/learning_journal/views/default.py
+++ learning_journal/learning_journal/views/default.py
@@ -1,7 +1,7 @@
 """Set up the default views."""
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPNotFound
-from data.entries import ENTRIES
+from learning_journal.views.data.entries import ENTRIES
 
 
 @view_config(route_name='list', renderer='../templates/list.jinja2')
@@ -9,11 +9,11 @@ def list_view(request):
     """Return index.html."""
     return {
         'page': 'Home',
-        'entry': 'entries'
+        'entry': ENTRIES
     }
 
 
-@view_config(route_name='list', renderer='../templates/detail.jinja2')
+@view_config(route_name='detail', renderer='../templates/detail.jinja2')
 def detail_view(request):
     """Return detail.html."""
     the_id = int(request.matchdict['id'])
@@ -27,7 +27,7 @@ def detail_view(request):
     }
 
 
-@view_config(route_name='list', renderer='../templates/new.jinja2')
+@view_config(route_name='create', renderer='../templates/new.jinja2')
 def create_view(request):
     """Return new.html."""
     return {
@@ -35,7 +35,7 @@ def create_view(request):
     }
 
 
-@view_config(route_name='list', renderer='../templates/edit.jinja2')
+@view_config(route_name='update', renderer='../templates/edit.jinja2')
 def update_view(request):
     """Return new.html."""
     the_id = int(request.matchdict['id'])
