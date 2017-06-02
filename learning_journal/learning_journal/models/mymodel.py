aec0a9e9b632514af616d045f337a95377aeeab3
Morgan Nomura <morganelle@gmail.com>
2017-06-02 15:02:52 -0700

    initial updates to populate data

--- learning_journal/learning_journal/models/mymodel.py
+++ learning_journal/learning_journal/models/mymodel.py
@@ -1,18 +1,18 @@
 from sqlalchemy import (
     Column,
-    Index,
     Integer,
-    Text,
+    Unicode
 )
 
 from .meta import Base
 
 
-class MyModel(Base):
-    __tablename__ = 'models'
-    id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
-
-
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+class JournalEntries(Base):
+    """Journal entry."""
+    
+    __tablename__ = 'journal'
+    id = Column(Integer, primary_key=True),
+    title = Column(Unicode),
+    text = Column(Unicode),
+    author = Column(Unicode),
+    date = Column(Unicode)
